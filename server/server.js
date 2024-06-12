d1ba70d5c067e0e2a82d7e184e8f07a83dcfb1b4
HoFFi02 <hoffman.michal02@gmail.com>
2024-06-12 14:27:24 +0200

    Dodanie bazy

--- server/server.js
+++ server/server.js
@@ -1,6 +1,25 @@
 // server/server.js
 const express = require('express');
+require('dotenv').config(); // Ładowanie zmiennych środowiskowych z pliku .env
 
+const mysql = require('mysql');
+
+// Utworzenie połączenia z bazą danych
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
+
+// Nawiązanie połączenia
+connection.connect((err) => {
+  if (err) {
+    console.error('Błąd połączenia z bazą danych:', err);
+    return;
+  }
+  console.log('Połączenie z bazą danych zostało nawiązane');
+});
 // Tworzenie instancji aplikacji Express
 const app = express();
 
